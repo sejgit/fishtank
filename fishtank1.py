21e10ee16781a6634df2c3a06d0ce9cc0c9413b8
sejgit <sejgit@github.com>
2016-07-05 10:09:47 -0400

    twrite to file2

--- fishtank1.py
+++ fishtank1.py
@@ -2,17 +2,23 @@
 # thomas & dad
 # 2016 06 20
 # 2016 06 21 update for logging
+# 2016 07 05 added temp prob & overlay to camera
 
 import schedule
 import time
 import datetime
 from gpiozero import LED
 import logging
+import os
+import glob
+import subprocess
 
+# get logging going
 logging.basicConfig(filename='fishtank/fishtank.log', format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
 logging.info('start program')
 
+# relay variables
 relay1 = LED(17)
 relay2 = LED(18)
 start = datetime.time(5, 30)
@@ -20,7 +26,35 @@ start_str = str(start)[:5]
 end = datetime.time(20, 45)
 end_str = str(end)[:5]
 
+# temp prob set-up and vars
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+# temp prob def
+def read_temp_raw():
+	catdata = subprocess.Popen(['cat',device_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out,err = catdata.communicate()
+	out_decode = out.decode('utf-8')
+	lines = out_decode.split('\n')
+	return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
+# relay def
 def relay1_on():
     relay1.off()
     logging.debug('relay1_on')
@@ -51,6 +85,14 @@ while True:
     schedule.run_pending()
     try:
         time.sleep(60) # wait one minute
+        deg_c, deg_f = read_temp()
+        if ast=="*":
+            ast = " "
+        else:
+            ast = "*"
+        with open('/dev/shm/mjpeg/user_annotate.txt', 'w') as f:
+            f.write('celcius {0:.2f}  fahrenheit {1:.2f}  {2}'.format(deg_c, deg_f, ast))
+        f.closed 
     except KeyboardInterrupt:
         print('\n\nKeyboard exception. Exiting.\n')
         logging.info('keyboard exception')
