6e540ac8682961bc1ec15ff875f5592ff13cfa1c
sejgit <sejgit@github.com>
2016-07-09 22:01:14 -0400

    stats-1

---
+++ temp_analize.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# fish tank temp data analizer
+# thomas & dad
+# 2016 07 08
+# todos: max mins and/or trends,
+# maybe: graph temps & overlay,
+
+### imports
+import csv
+
+"""
+import schedule
+import time
+import datetime
+from gpiozero import LED
+import logging
+import logging.handlers
+import os
+import sys
+import glob
+import subprocess
+"""
+### get logging going
+
+# set up a specific logger with desired output level
+LOG_FILENAME = '/home/pi/fishtank/temp_analize.log'
+logger = logging.getLogger('FishTankTempLogger')
+logger.setLevel(logging.DEBUG)
+
+# add the rotating log message handler
+fh = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=20000, backupCount=5)
+fh.setLevel(logging.DEBUG)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+fh.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(fh)
+
+logger.info('***start program')
+
+with open('/home/pi/fishtank/fishtemp.log') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=" ")
+    for row in readCSV:
+        print(row)
+        print(row[0])
+        print(row[0], row[1], row[2],)
+        
