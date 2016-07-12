3b3b71a7b1715878eef825b00a865fba08039d4e
sejgit <sejgit@github.com>
2016-07-11 21:16:09 -0400

    date analysis v1

--- temp_analize.py
+++ temp_analize.py
@@ -10,6 +10,9 @@ import csv
 import statistics
 import logging
 import logging.handlers
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 """
 import schedule
@@ -24,7 +27,7 @@ import subprocess
 ### get logging going
 
 # set up a specific logger with desired output level
-LOG_FILENAME = '/home/pi/fishtank/temp_analize.log'
+LOG_FILENAME = 'temp_analize.log'
 logger = logging.getLogger('FishTankTempLogger')
 logger.setLevel(logging.DEBUG)
 
@@ -41,39 +44,52 @@ logger.addHandler(fh)
 
 logger.info('***start program')
 
-with open('/home/pi/fishtank/fishtemp.log') as csvfile:
+#with open('/home/pi/fishtank/fishtemp.log') as csvfile:
+with open('data.example') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=" ")
     dates = []
     times = []
     temps_c = []
     temps_f = []
     statuss = []
+    dates0 = []
+    times0 = []
+    msgs0 = []
     for row in readCSV:
         if row[3] == 'celcius':
-            date = row[0]
-            time = row[1]
-            temp_c = float(row[4])
-            temp_f = float(row[7])
-            status = row[9]
-            dates.append(date)
-            times.append(time)
-            temps_c.append(temp_c)
-            temps_f.append(temp_f)
-            statuss.append(status)
+            dates.append(row[0])
+            times.append(row[1])
+            temps_c.append(float(row[4]))
+            temps_f.append(float(row[7]))
+            statuss.append(row[9])
         else:
-            print(row[3],)
+            dates0.append(row[0])
+            times0.append(row[1])
+            msgs0.append(row[3])
 
-    print(dates)
-    print(times)
-    print(temps_c)
-    print(temps_f)
-    print(statuss)
+#    print(dates)
+#    print(times)
+#    print(temps_c)
+#    print(temps_f)
+#    print(statuss)
     high = max(temps_f)
     low = min(temps_f)
     avg = statistics.mean(temps_f)
     var = statistics.variance(temps_f)
     std = statistics.stdev(temps_f)
     print(low, high, avg, var, std)
-    
+
+    t = np.arange(len(temps_f))
+    h = np.ones((len(temps_f)), dtype=int) * 76
+    l = np.ones((len(temps_f)), dtype=int) * 79
+    plt.title('Fish Tank Temperature')
+    plt.grid(True)
+    plt.ylabel('temp F')
+#    plt.axis([1, len(temps_f), 75, 80])
+    plt.ylim(75, 80)
+    plt.plot(t, temps_f, 'b-', t, l, 'r-', t, h,'r-')
+#    plt.annotate('
+    plt.show()
+
 
 
