c2fcfdc782d5bff391ee9f1c2baa4cfd9095c289
sejgit <sejgit@github.com>
2016-07-12 20:35:41 -0400

    move new log

---
+++ fishtemp.ex
@@ -0,0 +1,4 @@
+2016-07-12T20:23:39, INFO, ***start program
+2016-07-12T20:23:39, INFO, celcius, 25.75,  fahrenheit, 78.35,  ok
+2016-07-12T20:33:21, INFO, ***start program
+2016-07-12T20:33:21, INFO, celcius 25.81,  fahrenheit, 78.46,  ok
