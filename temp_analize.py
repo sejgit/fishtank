9ebac9d30f947a838cea74cedcce89c89b0e0922
sejgit <sejgit@github.com>
2016-07-14 13:10:10 -0400

    up

--- temp_analize.py
+++ temp_analize.py
@@ -2,13 +2,12 @@
 # fish tank temp data analizer
 # thomas & dad
 # 2016 07 08
+# 2016 07 14 using pandas
 # todos: max mins and/or trends,
 # maybe: graph temps & overlay,
 
 ### imports
 import datetime as dt
-import csv
-import statistics
 import logging
 import logging.handlers
 import pandas as pd
@@ -41,14 +40,17 @@ logger.addHandler(fh)
 logger.info('***start program')
 
 
-### pull in log fishtemp.log file into panda
-pdinp = pd.read_csv('fishtemp.log', header=None, names=['date', 'type', 'status', 'temp_C', 'temp_F'], index_col=0, parse_dates = True)
+### pull in log fishtemp.log file into pandas
+pdinp = pd.read_csv('fishtemp.log', header=None, names=['datestamp', 'type', 'status', 'temp_C', 'temp_F'], index_col=0, parse_dates = True)
 print(pdinp)
 print(pdinp.index)
-pdinp = pd.DataFrame(pdinp)
-pdinp.plot(pdinp)
-plt.savefig('test.png')
 
+
+
+pdinp.plot()
+fig = plt.gcf()
+fig.savefig('plot.png')
+plt.show()
 
 
 
