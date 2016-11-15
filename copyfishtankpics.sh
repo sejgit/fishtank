f301053edab8907ec59b8717815e560859b46f5b
sejgit <sejgit@github.com>
2016-11-14 19:57:07 -0500

    up

--- copyfishtankpics.sh
+++ copyfishtankpics.sh
@@ -6,7 +6,10 @@
 
 # 2016 11 14
 
-sshpass -f /home/pi/.ssh/sshpass scp /home/pi/fishtank/plot.png pi@pi3:/home/pi/fishdata/plot.png
+sshpass -f /home/pi/.ssh/sshpass scp /home/pi/fishtank/plot.png pi@pi3:/home/pi/fishdata/
+sshpass -f /home/pi/.ssh/sshpass scp /run/shm/mjpeg/user_annotate.txt pi@pi3:/home/pi/fishdata/
+sshpass -f /home/pi/.ssh/sshpass scp /run/shm/mjpeg/cam.jpg pi@pi3:/home/pi/fishdata/
+
 
 
 
