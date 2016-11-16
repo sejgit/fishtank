1e3028ddc231caa070b7591acc67398ca11afd8c
sejgit <sejgit@github.com>
2016-11-16 15:10:00 -0500

    up

--- copyfishtankpics.sh
+++ copyfishtankpics.sh
@@ -7,10 +7,12 @@
 # 2016 11 14
 # 2016 11 16
 
+cat /home/pi/.ssh/id_rsa | SSH_ASKPASS="/home/pi/.ssh/sshpassfile.sh" ssh-add -
 fuser -k /usr/bin/sshpass
 sshpass -f /home/pi/.ssh/sshpassfile scp /home/pi/fishtank/plot.png pi@pi3:/home/pi/fishtank/fishwww/
 sshpass -f /home/pi/.ssh/sshpassfile scp /run/shm/mjpeg/user_annotate.txt pi@pi3:/home/pi/fishtank/fishwww/
-sshpass -f /home/pi/.ssh/sshpassfilescp /run/shm/mjpeg/cam.jpg pi@pi3:/home/pi/fishtank/fishwww/
+sshpass -f /home/pi/.ssh/sshpassfile scp /run/shm/mjpeg/cam.jpg pi@pi3:/home/pi/fishtank/fishwww/
+ssh-add -D
 
 
 
