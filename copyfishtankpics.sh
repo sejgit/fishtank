c2d4307edcc5de43b0ac26cac42ee9c4ceb79103
sejgit <sejgit@github.com>
2016-12-22 07:20:19 -0500

    change to gpio & one channel

--- copyfishtankpics.sh
+++ copyfishtankpics.sh
@@ -7,7 +7,7 @@
 # 2016 11 14
 # 2016 11 16
 
-eval $(ssh-agent)
+#eval $(ssh-agent)
 cat /home/pi/.ssh/id_rsa | SSH_ASKPASS="/home/pi/.ssh/sshpassfile.sh" ssh-add -
 # fuser -k /usr/bin/sshpass
 # sshpass -f /home/pi/.ssh/sshpassfile scp /home/pi/fishtank/plot.png pi@pi3:/home/pi/fishtank/fishwww/
@@ -16,7 +16,7 @@ cat /home/pi/.ssh/id_rsa | SSH_ASKPASS="/home/pi/.ssh/sshpassfile.sh" ssh-add -
 scp /home/pi/fishtank/plot.png pi@pi3:/home/pi/fishtank/fishwww/
 scp /run/shm/mjpeg/user_annotate.txt pi@pi3:/home/pi/fishtank/fishwww/
 scp /run/shm/mjpeg/cam.jpg pi@pi3:/home/pi/fishtank/fishwww/
-ssh-add -D
+#ssh-add -D
 
 
 
