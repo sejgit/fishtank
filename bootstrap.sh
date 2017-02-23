493efeddc6532dc53edb1622cdbeb41e46bbfec3
sejgit <sejgit@github.com>
2017-02-22 20:47:09 -0500

    boot update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # bootstrap
 
-sudo apt-get install python3-matplotlib python3-pandas
+sudo apt-get install python3-matplotlib python3-pandas python-rpi.gpio
 sudo pip3 install schedule
